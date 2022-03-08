4e5c7bf5ec2c15072931ca0f53b59e6fcc55b356
Chris Morris <cmorris@Chriss-iMac-2.local>
2022-03-08 16:35:12 +0000

    Adds stock lookup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,12 @@
 class HomeController < ApplicationController
+  def index
+    @api = StockQuote::Stock.new(api_key: 'pk_bbe95e67cb694c3a9c7e3cf55ef9f2e8')
+
+
+    if params[:ticker] != nil
+      @ticker = params[:ticker]
+      @stock = StockQuote::Stock.quote(@ticker)
+      debugger
+    end
+  end
 end
