f2c55c3b7698f9addb84868eacf00810ed187795
Chris Morris <cmorris@Chriss-iMac-2.local>
2022-03-10 16:17:29 +0000

    Added stock api calls to display live data

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,7 +5,21 @@ class StocksController < ApplicationController
 
   # GET /stocks or /stocks.json
   def index
-    @stocks = Stock.all
+    @stocks = current_user.stocks
+
+    @api = StockQuote::Stock.new(api_key: 'pk_bbe95e67cb694c3a9c7e3cf55ef9f2e8')
+
+
+    # if params[:ticker] != nil
+    #   @ticker = params[:ticker]
+    #   begin
+    #     @stock = StockQuote::Stock.quote(@ticker)
+    #   rescue => exception
+    #     @failed_to_fetch = "Ticker symbol doesn't exist"
+    #   end
+    # elsif
+    #   @nothing = "Hey, you forgot to enter a symbol"
+    # end
   end
 
   # GET /stocks/1 or /stocks/1.json
